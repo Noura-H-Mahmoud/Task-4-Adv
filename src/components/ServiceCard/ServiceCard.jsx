5277bcc136a188c5c1ceaa6c4731616f8d710cb3
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-09-12 22:19:15 +0400

    the last commit

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -5,7 +5,7 @@ export default function ServiceCard({ title, text, icon: Icon }) {
     return (
         <Card className='h-100 border-0 text-color'>
             <Card.Body className='d-flex flex-column align-items-center text-center gap-3'>
-                <div className='color-main box-shdow max-content py-4 px-4 rounded-circle'>
+                <div className='color-main box-shdow max-content py-4 px-4 rounded-circle box-shadow'>
                     <Icon size={40} />
                 </div>
                 <Card.Title className='fs-4 after-title position-relative'>{title}</Card.Title>
